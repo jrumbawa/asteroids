7efa5b38f0e72300b5eaa1b110c5570101037320
Jomar Rumbawa <hello@jomarrumbawa.com>
2025-01-25 10:24:52 -0800

    Add player rotation

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ def main():
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
   dt = 0
+
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
     screen.fill((0, 0, 0))
+    player.update(dt)
     player.draw(screen)
     pygame.display.flip()
     dt = clock.tick(60) / 1000
