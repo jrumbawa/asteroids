07deb6e7017bfc85731c51dd2de1df00110ec68c
Jomar Rumbawa <hello@jomarrumbawa.com>
2025-01-26 23:10:08 -0800

    Create astero and asteroidfield

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
   pygame.init()
@@ -16,6 +18,11 @@ def main():
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
   Player.containers = (updatable, drawable)
+  asteroids = pygame.sprite.Group()
+  Asteroid.containers = (asteroids, updatable, drawable)
+  AsteroidField.containers = (updatable,)
+  asteroid_field = AsteroidField()
+
 
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   while True:
