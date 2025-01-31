f5cd69ce188339e509f90b142d5d05c705a7533e
Jomar Rumbawa <hello@jomarrumbawa.com>
2025-01-31 10:42:56 -0800

    Create asteroid killing and splitting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -34,6 +35,18 @@ def main():
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+      
+    updatable.update(dt)
+      
+    for asteroid in asteroids:
+      if asteroid.collides_with(player):
+          print("Game over!")
+          sys.exit()
+
+      for shot in shots:
+          if asteroid.collides_with(shot):
+              shot.kill()
+              asteroid.split()
     screen.fill((0, 0, 0))
     for item in updatable:
       item.update(dt)
